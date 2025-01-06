33370278449152c3d66e024e791adbff94035df3
yasunoh <checkupmaster@gmail.com>
2025-01-06 14:58:29 +0900

    update header menu

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import ModeToggle from "./mode-toggle";
 import Link from "next/link";
-import { EllipsisVertical, ShoppingCart, UserIcon } from "lucide-react";
+import { EllipsisVertical, ShoppingCart } from "lucide-react";
 import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import UserButton from "./user-button";
 
