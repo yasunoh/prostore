832a89ebababbf99793fa3ee8c15194a823dcc97
yasunoh <checkupmaster@gmail.com>
2025-02-21 14:05:04 +0900

    update homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,22 @@
 import ProductList from "@/components/shared/product/poduct-list";
+import ProductCarousel from "@/components/shared/product/product-carousel";
+import ViewAllProductsButton from "@/components/view-all-products-button";
 // import sampleData from "@/db/sample-data";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import { getFeaturedProducts, getLatestProducts } from "@/lib/actions/product.actions";
 // const delay = (ms:number) => new Promise((resolve) => setTimeout(resolve, ms))
 
 
 const Homepage = async() => {
   // console.log(sampleData)
   // await delay(2000)
-  const latestProducts = await getLatestProducts()
+  const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
+  
 
   return <>
+    {featuredProducts.length > 0 && <ProductCarousel data={featuredProducts} />}
     <ProductList data={latestProducts} title="Newest Arrivals" />
+    <ViewAllProductsButton />
   </>;
 }
  
--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -2,12 +2,15 @@ import Image from "next/image";
 import Link from "next/link";
 import { APP_NAME } from "@/lib/constants";
 import Menu from "./menu";
+import CategoryDrawer from "./category-draw";
+import Search from "./search";
 
 const Header = () => {
   return <header className="w-full border-b">
     <div className="wrapper flex-between">
       <div className="flex-start">
-        <Link href='/' className="flex-start">
+        <CategoryDrawer />
+        <Link href='/' className="flex-start ml-4">
           <Image src='/images/logo.svg' alt={`${APP_NAME} logo`}
             height={48} width={48} priority={true} 
           />
@@ -15,6 +18,9 @@ const Header = () => {
             {APP_NAME}
           </span>
         </Link>
+      </div>
+      <div className="hidden md:block">
+        <Search />
       </div>
       <Menu />
     </div>
